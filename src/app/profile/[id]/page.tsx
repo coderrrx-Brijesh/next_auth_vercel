ec548ca89ff20e7c9728cea2fd01940e5a42fc41
Brijesh Singh <coderrrx@gmail.com>
2025-03-19 15:52:14 +0530

    build correction 5

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React from "react";
 
-const ProfilePage = async ({ params }: { params: { id: string } }) => {
-  const { id } = await params // need to await params
+const ProfilePage = ({ params }: { params: { id: string } }) => {
+  const { id } = params;
+
   return (
     <div>
       <div>Welcome to Profile of</div>
-      <span className='bg-amber-600 rounded-b-md text-black p-2'>{id}</span>
+      <span className="bg-amber-600 rounded-b-md text-black p-2">{id}</span>
     </div>
-  )
-}
+  );
+};
 
-export default ProfilePage
+export default ProfilePage;
